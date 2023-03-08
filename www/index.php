5f6781c03d6e3e8b9958a9c714beeb0ceea7a1dd
GanaaChimgee <ganchimeg.ghana0815@gmail.com>
2023-03-08 19:17:42 +0100

    update main cofif

--- www/index.php
+++ www/index.php
@@ -5,5 +5,11 @@ const VIEWS_PATH = ROOT . DS . 'views' . DS;
 
 require_once ROOT . DS . 'lib' . DS . 'init.php';
 
+
+// Session::setMessage("wrong password...");
+
 $app = new App();
 $app->run($_SERVER['REQUEST_URI']);
+// $rows = App::$db->query("select * from product");
+// echo "<pre>";
+// print_r($rows);
