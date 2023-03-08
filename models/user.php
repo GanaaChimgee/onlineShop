cab1187764e9a5f8450c7c9b7b10005a57fb558e
GanaaChimgee <ganchimeg.ghana0815@gmail.com>
2023-03-08 19:17:10 +0100

    update models

--- models/user.php
+++ models/user.php
@@ -6,7 +6,36 @@ class User extends Model
     public function getByLogin($login)
     {
         $login = $this->db->escape($login);
-        $sql = "select * from users where login='$login' limit 1";
+        $sql = "select * from customer where login='$login' limit 1";
         return $this->db->query($sql);
+    }
+
+    public function handleLogin(string $email, string $password): bool
+    {
+        $sql = "SELECT * FROM customer WHERE email = '%s'";
+        $user = $this->db->query(sprintf($sql, $email));
+
+        $passwordOk = password_verify($password, $user[0]["PASSWORD"]);
+        //$passwordOk = $password == $user[0]["PASSWORD"];
+
+        if ($passwordOk === true) {
+            // $this->ensureSession();
+            //session_regenerate_id();
+            //$_SESSION['adminLogin'] = $username;
+            Session::set('user', $user[0]);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function handleRegister(string $email, string $pwd, $name): bool
+    {
+        $password = password_hash($pwd, PASSWORD_BCRYPT);
+        $sql = "INSERT INTO customer(email, password, name) values('$email', '$password', ' $name')";
+        $this->db->query($sql);
+        Session::delete('user');
+
+        return true;
     }
 }
